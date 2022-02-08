5d61cb5015f169594c3937e4944e1e8734a5c6a8
Peter Levinsky <pele@easj.dk>
2022-02-08 10:47:13 +0100

    Add project files.

---
+++ ScrumBoard/Services/UserStoryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScrumBoard.MockData;
+using ScrumBoardLib.model;
+
+namespace ScrumBoard.Services
+{
+    public class UserStoryService
+    {
+        private readonly List<UserStory> _userStories;
+
+        public UserStoryService()
+        {
+            _userStories = MockUserStories.GetMockUserStories();
+        }
+
+        public List<UserStory> GetUserStories()
+        {
+            return new List<UserStory>(_userStories);
+        }
+    }
+}
